b14788bc5df5b895f4b03c697b83ff5e2b847cd2
Maria Timmerholm <timmerholmmaria@gmail.com>
2026-03-04 12:54:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ const observer = new IntersectionObserver(
         document.body.className = "";
 
         if (entry.target.classList.contains("era-1")) {
-          document.body.classList.add("industrialism");
+          document.body.classList.add("Industrialism");
         }
         
         if (entry.target.classList.contains("era-2")) {
